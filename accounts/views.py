5c4c09e485c1db9795ee33edd757e975c4e0a9c3
KimGyoungmin <min199485@gmail.com>
2024-09-11 17:54:01 +0900

    ProfileFeature:단순정보조회 현재 유효성 검사는 넣지않은  상태이며 프로필 페이지 필드는 회원가입 기능이 완료가 되면 넣도록 할게요

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from .models import User
+from .serializers import UserProfileSerializers
+from rest_framework.response import Response
 
-# Create your views here.
+
+class UserProfileView(APIView):
+    def get(self, request, account_id):
+        user = get_object_or_404(User, pk=account_id)
+        serializer = UserProfileSerializers(user)
+        
+        return Response(serializer.data)
