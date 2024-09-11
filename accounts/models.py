d740d727ad78945d4b14ea67000c6e167471d823
l-yoon <min>
2024-09-11 18:59:11 +0900

    models.py/nickname=unique added

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     gender = (('M', 'Male'), ('F', 'Female'), ('O', 'Other'))
     username = models.CharField(max_length=50, unique=True)
-    nickname = models.CharField(max_length=50, null=True, blank=True)
+    nickname = models.CharField(max_length=50, null=True, blank=True, unique=True)
     birth = models.DateField()
     gender = models.CharField(choices=gender, max_length=1, other='O')
     bio = models.TextField(null=True, blank=True)
