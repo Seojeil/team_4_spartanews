2271b268c81ed7ab456170ce1de7cd75ddb36456
jeil <sjl01002@gmail.com>
2024-09-12 15:36:36 +0900

    feat:팔로우 기능 생성

--- accounts/models.py
+++ accounts/models.py
@@ -3,12 +3,19 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    gender = (('M', 'Male'), ('F', 'Female'), ('O', 'Other'),)
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+        )
+    
     username = models.CharField(max_length=50, unique=True)
     nickname = models.CharField(
         max_length=50, null=True, blank=True, unique=True)
     birth = models.DateField()
-    gender = models.CharField(choices=gender, max_length=1, default='O')
+    gender = models.CharField(choices=GENDER, max_length=1, default='O')
+    followings = models.ManyToManyField(
+        'self', symmetrical=False, related_name='followers')
     bio = models.TextField(null=True, blank=True)
 
     def delete(self, using=None, keep_parents=False):
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import get_object_or_404
-from rest_framework.views import APIView
 from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.exceptions import TokenError
-from rest_framework import status
 from .validators import validate_signup
 from .models import User
 from .serializers import (
@@ -36,6 +36,7 @@ class SignupView(APIView):
     def delete(self, request):
         user = request.user
         password = request.data.get("password")
+        
         if user.check_password(password):
             user.delete()
             return Response({"message": "회원 비활성화"})
@@ -44,22 +45,41 @@ class SignupView(APIView):
 
 
 class UserProfileView(APIView):
-    # 프로필 조회
     permission_classes = [IsAuthenticated]
+    
+    def get_object(self, account_id):
+        return get_object_or_404(User, pk=account_id)
 
+    # 프로필 조회
     def get(self, request, account_id):
-        user = get_object_or_404(User, pk=account_id)
+        user = self.get_object(account_id)
         serializer = UserProfileSerializers(user)
         return Response(serializer.data)
 
+    # 팔로우
+    def post(self, request, account_id):
+        user = self.get_object(account_id)
+        
+        if user == request.user:
+            return Response({"error": "자신을 팔로우할 수 없습니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if user.followers.filter(pk=request.user.pk).exists():
+            user.followers.remove(request.user)
+            return Response({"detail": "팔로우가 취소되었습니다."}, status=status.HTTP_200_OK)
+        else:
+            user.followers.add(request.user)
+            return Response({"detail": "팔로우되었습니다."}, status=status.HTTP_200_OK)
+    
     # 프로필 수정
     def put(self, request, account_id):
         # 1. 특정 회원정보
         user = request.user
+        
         if user.id != account_id:
             return Response({"message": "회원 정보와 일치하지 않습니다."}, status=status.HTTP_401_UNAUTHORIZED)
         serializers = UserUpdateSerializers(
             instance=user, data=request.data, partial=True)
+        
         if serializers.is_valid(raise_exception=True):
             serializers.save()
             return Response(serializers.data)
@@ -72,8 +92,10 @@ class UserChangePasswordView(APIView):
         # 비밀번호 변경
         if request.data.get("prev_password") == request.data.get("password_1"):
             return Response({"message": "기존의 비밀번호와 일치합니다."}, status=status.HTTP_400_BAD_REQUEST)
+        
         serializer = UserChangePasswordSerailizers(
             instance=request.user, data=request.data)
+        
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response({"Message": "비밀번호변경완료"})
@@ -86,6 +108,7 @@ class UserLoginView(APIView):
         username = request.data.get("username")
         password = request.data.get("password")
         user = authenticate(request, username=username, password=password)
+        
         if user is not None:
             refresh = RefreshToken.for_user(user)
             allowed_id = {
@@ -93,8 +116,8 @@ class UserLoginView(APIView):
                 'refresh': str(refresh),
                 'access': str(refresh.access_token)
             }
-
             return Response(allowed_id)
+        
         else:
             return Response({"message": "아이디 또는 비밀번호가 일치하지않습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
@@ -108,7 +131,6 @@ class LogoutView(APIView):
             refresh_token = request.data["refresh_token"]
             token = RefreshToken(refresh_token)
             token.blacklist()
-
             return Response({"detail": "로그아웃되었습니다."}, status=status.HTTP_200_OK)
 
         except TokenError:
