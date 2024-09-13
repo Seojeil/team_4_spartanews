1d9ffd8532070f99d35d64c925ee748a6c74a7f9
l-yoon <min>
2024-09-13 22:01:56 +0900

    changed settings.py->allowed_hosts

--- spartanews/settings.py
+++ spartanews/settings.py
@@ -27,7 +27,11 @@ SECRET_KEY = config.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "13.125.62.89",
+    ]
 
 
 # Application definition
